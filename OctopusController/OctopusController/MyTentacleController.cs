25835201ccbf8b072face8a7bc5b0eb345163636
Abel Pujol <abelpujol99@gmail.com>
2023-11-21 19:30:29 +0100

    Tentacle Controller Almost Done

--- OctopusController/OctopusController/MyTentacleController.cs
+++ OctopusController/OctopusController/MyTentacleController.cs
@@ -25,22 +25,45 @@ namespace OctopusController
         //Exercise 1.
         public Transform[] LoadTentacleJoints(Transform root, TentacleMode mode)
         {
+            
             //TODO: add here whatever is needed to find the bones forming the tentacle for all modes
             //you may want to use a list, and then convert it to an array and save it into _bones
             tentacleMode = mode;
 
             switch (tentacleMode){
                 case TentacleMode.LEG:
-                    //TODO: in _endEffectorsphere you keep a reference to the base of the leg
+
+                    LoadJoints(2, root);
+                    
                     break;
+                
                 case TentacleMode.TAIL:
-                    //TODO: in _endEffectorsphere you keep a reference to the red sphere 
+
+                    //LoadJoints(4, root);
                     break;
+                
                 case TentacleMode.TENTACLE:
                     //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
                     break;
             }
             return Bones;
+        }
+
+        private void LoadJoints(int jointsLength, Transform root)
+        {
+            List<Transform> transformList = new List<Transform> { root };
+            
+            //Debug.Log(root.name);
+            
+            for (int i = 0; i < jointsLength; i++)
+            {
+                transformList.Add(transformList[i].GetChild(1));
+                //Debug.Log(transformList[i].name);
+            }
+            _endEffectorSphere = transformList[transformList.Count - 1].GetChild(1);
+            //Debug.Log(_endEffectorSphere.name);
+                    
+            _bones = transformList.ToArray();
         }
     }
 }
